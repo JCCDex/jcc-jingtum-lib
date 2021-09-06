8ec89c3ec9c54eeee87020039f8f1c7018892d7e
GinMu <gin.musheng@gmail.com>
2021-09-06 10:24:03 +0800

    generate doc

---
+++ docs/assets/js/search.js
@@ -0,0 +1,474 @@
+window.searchData = {
+  kinds: { 128: "Class", 256: "Interface", 512: "Constructor", 1024: "Property", 2048: "Method" },
+  rows: [
+    { id: 0, kind: 256, name: "IPayment", url: "interfaces/IPayment.html", classes: "tsd-kind-interface" },
+    {
+      id: 1,
+      kind: 1024,
+      name: "address",
+      url: "interfaces/IPayment.html#address",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 2,
+      kind: 1024,
+      name: "secret",
+      url: "interfaces/IPayment.html#secret",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 3,
+      kind: 1024,
+      name: "to",
+      url: "interfaces/IPayment.html#to",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 4,
+      kind: 1024,
+      name: "token",
+      url: "interfaces/IPayment.html#token",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 5,
+      kind: 1024,
+      name: "amount",
+      url: "interfaces/IPayment.html#amount",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 6,
+      kind: 1024,
+      name: "memo",
+      url: "interfaces/IPayment.html#memo",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    {
+      id: 7,
+      kind: 1024,
+      name: "issuer",
+      url: "interfaces/IPayment.html#issuer",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "IPayment"
+    },
+    { id: 8, kind: 256, name: "ChainOption", url: "interfaces/ChainOption.html", classes: "tsd-kind-interface" },
+    {
+      id: 9,
+      kind: 1024,
+      name: "guomi",
+      url: "interfaces/ChainOption.html#guomi",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ChainOption"
+    },
+    {
+      id: 10,
+      kind: 1024,
+      name: "ACCOUNT_ALPHABET",
+      url: "interfaces/ChainOption.html#ACCOUNT_ALPHABET",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ChainOption"
+    },
+    {
+      id: 11,
+      kind: 1024,
+      name: "currency",
+      url: "interfaces/ChainOption.html#currency",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ChainOption"
+    },
+    {
+      id: 12,
+      kind: 1024,
+      name: "fee",
+      url: "interfaces/ChainOption.html#fee",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ChainOption"
+    },
+    { id: 13, kind: 256, name: "SignResult", url: "interfaces/SignResult.html", classes: "tsd-kind-interface" },
+    {
+      id: 14,
+      kind: 1024,
+      name: "hash",
+      url: "interfaces/SignResult.html#hash",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "SignResult"
+    },
+    {
+      id: 15,
+      kind: 1024,
+      name: "blob",
+      url: "interfaces/SignResult.html#blob",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "SignResult"
+    },
+    { id: 16, kind: 128, name: "Wallet", url: "classes/Wallet.html", classes: "tsd-kind-class" },
+    {
+      id: 17,
+      kind: 512,
+      name: "constructor",
+      url: "classes/Wallet.html#constructor",
+      classes: "tsd-kind-constructor tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    {
+      id: 18,
+      kind: 1024,
+      name: "wallet",
+      url: "classes/Wallet.html#wallet",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-protected",
+      parent: "Wallet"
+    },
+    {
+      id: 19,
+      kind: 1024,
+      name: "serializer",
+      url: "classes/Wallet.html#serializer",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-protected",
+      parent: "Wallet"
+    },
+    {
+      id: 20,
+      kind: 2048,
+      name: "getAddress",
+      url: "classes/Wallet.html#getAddress",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    {
+      id: 21,
+      kind: 2048,
+      name: "isValidAddress",
+      url: "classes/Wallet.html#isValidAddress",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    {
+      id: 22,
+      kind: 2048,
+      name: "isValidSecret",
+      url: "classes/Wallet.html#isValidSecret",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    {
+      id: 23,
+      kind: 2048,
+      name: "createWallet",
+      url: "classes/Wallet.html#createWallet",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    {
+      id: 24,
+      kind: 2048,
+      name: "sign",
+      url: "classes/Wallet.html#sign",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Wallet"
+    },
+    { id: 25, kind: 128, name: "Transaction", url: "classes/Transaction.html", classes: "tsd-kind-class" },
+    {
+      id: 26,
+      kind: 2048,
+      name: "fetchSequence",
+      url: "classes/Transaction.html#fetchSequence",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "Transaction"
+    },
+    {
+      id: 27,
+      kind: 2048,
+      name: "fetchTransaction",
+      url: "classes/Transaction.html#fetchTransaction-1",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "Transaction"
+    },
+    {
+      id: 28,
+      kind: 2048,
+      name: "isValidated",
+      url: "classes/Transaction.html#isValidated",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "Transaction"
+    },
+    {
+      id: 29,
+      kind: 2048,
+      name: "isSuccess",
+      url: "classes/Transaction.html#isSuccess",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "Transaction"
+    },
+    {
+      id: 30,
+      kind: 512,
+      name: "constructor",
+      url: "classes/Transaction.html#constructor",
+      classes: "tsd-kind-constructor tsd-parent-kind-class tsd-is-overwrite",
+      parent: "Transaction"
+    },
+    {
+      id: 31,
+      kind: 1024,
+      name: "nodes",
+      url: "classes/Transaction.html#nodes",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "Transaction"
+    },
+    {
+      id: 32,
+      kind: 2048,
+      name: "getNode",
+      url: "classes/Transaction.html#getNode",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-protected",
+      parent: "Transaction"
+    },
+    {
+      id: 33,
+      kind: 2048,
+      name: "buildPayment",
+      url: "classes/Transaction.html#buildPayment",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Transaction"
+    },
+    {
+      id: 34,
+      kind: 2048,
+      name: "fetchTransaction",
+      url: "classes/Transaction.html#fetchTransaction",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Transaction"
+    },
+    {
+      id: 35,
+      kind: 2048,
+      name: "payment",
+      url: "classes/Transaction.html#payment",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "Transaction"
+    },
+    {
+      id: 36,
+      kind: 1024,
+      name: "wallet",
+      url: "classes/Transaction.html#wallet",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-inherited tsd-is-protected",
+      parent: "Transaction"
+    },
+    {
+      id: 37,
+      kind: 1024,
+      name: "serializer",
+      url: "classes/Transaction.html#serializer",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-inherited tsd-is-protected",
+      parent: "Transaction"
+    },
+    {
+      id: 38,
+      kind: 2048,
+      name: "getAddress",
+      url: "classes/Transaction.html#getAddress",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+      parent: "Transaction"
+    },
+    {
+      id: 39,
+      kind: 2048,
+      name: "isValidAddress",
+      url: "classes/Transaction.html#isValidAddress",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+      parent: "Transaction"
+    },
+    {
+      id: 40,
+      kind: 2048,
+      name: "isValidSecret",
+      url: "classes/Transaction.html#isValidSecret",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+      parent: "Transaction"
+    },
+    {
+      id: 41,
+      kind: 2048,
+      name: "createWallet",
+      url: "classes/Transaction.html#createWallet",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+      parent: "Transaction"
+    },
+    {
+      id: 42,
+      kind: 2048,
+      name: "sign",
+      url: "classes/Transaction.html#sign",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-inherited",
+      parent: "Transaction"
+    }
+  ],
+  index: {
+    version: "2.3.9",
+    fields: ["name", "parent"],
+    fieldVectors: [
+      ["name/0", [0, 16.441]],
+      ["parent/0", []],
+      ["name/1", [1, 33.787]],
+      ["parent/1", [0, 1.56]],
+      ["name/2", [2, 33.787]],
+      ["parent/2", [0, 1.56]],
+      ["name/3", [3, 33.787]],
+      ["parent/3", [0, 1.56]],
+      ["name/4", [4, 33.787]],
+      ["parent/4", [0, 1.56]],
+      ["name/5", [5, 33.787]],
+      ["parent/5", [0, 1.56]],
+      ["name/6", [6, 33.787]],
+      ["parent/6", [0, 1.56]],
+      ["name/7", [7, 33.787]],
+      ["parent/7", [0, 1.56]],
+      ["name/8", [8, 20.794]],
+      ["parent/8", []],
+      ["name/9", [9, 33.787]],
+      ["parent/9", [8, 1.973]],
+      ["name/10", [10, 33.787]],
+      ["parent/10", [8, 1.973]],
+      ["name/11", [11, 33.787]],
+      ["parent/11", [8, 1.973]],
+      ["name/12", [12, 33.787]],
+      ["parent/12", [8, 1.973]],
+      ["name/13", [13, 25.314]],
+      ["parent/13", []],
+      ["name/14", [14, 33.787]],
+      ["parent/14", [13, 2.402]],
+      ["name/15", [15, 33.787]],
+      ["parent/15", [13, 2.402]],
+      ["name/16", [16, 13.418]],
+      ["parent/16", []],
+      ["name/17", [17, 28.679]],
+      ["parent/17", [16, 1.273]],
+      ["name/18", [16, 13.418]],
+      ["parent/18", [16, 1.273]],
+      ["name/19", [18, 28.679]],
+      ["parent/19", [16, 1.273]],
+      ["name/20", [19, 28.679]],
+      ["parent/20", [16, 1.273]],
+      ["name/21", [20, 28.679]],
+      ["parent/21", [16, 1.273]],
+      ["name/22", [21, 28.679]],
+      ["parent/22", [16, 1.273]],
+      ["name/23", [22, 28.679]],
+      ["parent/23", [16, 1.273]],
+      ["name/24", [23, 28.679]],
+      ["parent/24", [16, 1.273]],
+      ["name/25", [24, 8.664]],
+      ["parent/25", []],
+      ["name/26", [25, 33.787]],
+      ["parent/26", [24, 0.822]],
+      ["name/27", [26, 28.679]],
+      ["parent/27", [24, 0.822]],
+      ["name/28", [27, 33.787]],
+      ["parent/28", [24, 0.822]],
+      ["name/29", [28, 33.787]],
+      ["parent/29", [24, 0.822]],
+      ["name/30", [17, 28.679]],
+      ["parent/30", [24, 0.822]],
+      ["name/31", [29, 33.787]],
+      ["parent/31", [24, 0.822]],
+      ["name/32", [30, 33.787]],
+      ["parent/32", [24, 0.822]],
+      ["name/33", [31, 33.787]],
+      ["parent/33", [24, 0.822]],
+      ["name/34", [26, 28.679]],
+      ["parent/34", [24, 0.822]],
+      ["name/35", [32, 33.787]],
+      ["parent/35", [24, 0.822]],
+      ["name/36", [16, 13.418]],
+      ["parent/36", [24, 0.822]],
+      ["name/37", [18, 28.679]],
+      ["parent/37", [24, 0.822]],
+      ["name/38", [19, 28.679]],
+      ["parent/38", [24, 0.822]],
+      ["name/39", [20, 28.679]],
+      ["parent/39", [24, 0.822]],
+      ["name/40", [21, 28.679]],
+      ["parent/40", [24, 0.822]],
+      ["name/41", [22, 28.679]],
+      ["parent/41", [24, 0.822]],
+      ["name/42", [23, 28.679]],
+      ["parent/42", [24, 0.822]]
+    ],
+    invertedIndex: [
+      ["account_alphabet", { _index: 10, name: { 10: {} }, parent: {} }],
+      ["address", { _index: 1, name: { 1: {} }, parent: {} }],
+      ["amount", { _index: 5, name: { 5: {} }, parent: {} }],
+      ["blob", { _index: 15, name: { 15: {} }, parent: {} }],
+      ["buildpayment", { _index: 31, name: { 33: {} }, parent: {} }],
+      ["chainoption", { _index: 8, name: { 8: {} }, parent: { 9: {}, 10: {}, 11: {}, 12: {} } }],
+      ["constructor", { _index: 17, name: { 17: {}, 30: {} }, parent: {} }],
+      ["createwallet", { _index: 22, name: { 23: {}, 41: {} }, parent: {} }],
+      ["currency", { _index: 11, name: { 11: {} }, parent: {} }],
+      ["fee", { _index: 12, name: { 12: {} }, parent: {} }],
+      ["fetchsequence", { _index: 25, name: { 26: {} }, parent: {} }],
+      ["fetchtransaction", { _index: 26, name: { 27: {}, 34: {} }, parent: {} }],
+      ["getaddress", { _index: 19, name: { 20: {}, 38: {} }, parent: {} }],
+      ["getnode", { _index: 30, name: { 32: {} }, parent: {} }],
+      ["guomi", { _index: 9, name: { 9: {} }, parent: {} }],
+      ["hash", { _index: 14, name: { 14: {} }, parent: {} }],
+      ["ipayment", { _index: 0, name: { 0: {} }, parent: { 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {} } }],
+      ["issuccess", { _index: 28, name: { 29: {} }, parent: {} }],
+      ["issuer", { _index: 7, name: { 7: {} }, parent: {} }],
+      ["isvalidaddress", { _index: 20, name: { 21: {}, 39: {} }, parent: {} }],
+      ["isvalidated", { _index: 27, name: { 28: {} }, parent: {} }],
+      ["isvalidsecret", { _index: 21, name: { 22: {}, 40: {} }, parent: {} }],
+      ["memo", { _index: 6, name: { 6: {} }, parent: {} }],
+      ["nodes", { _index: 29, name: { 31: {} }, parent: {} }],
+      ["payment", { _index: 32, name: { 35: {} }, parent: {} }],
+      ["secret", { _index: 2, name: { 2: {} }, parent: {} }],
+      ["serializer", { _index: 18, name: { 19: {}, 37: {} }, parent: {} }],
+      ["sign", { _index: 23, name: { 24: {}, 42: {} }, parent: {} }],
+      ["signresult", { _index: 13, name: { 13: {} }, parent: { 14: {}, 15: {} } }],
+      ["to", { _index: 3, name: { 3: {} }, parent: {} }],
+      ["token", { _index: 4, name: { 4: {} }, parent: {} }],
+      [
+        "transaction",
+        {
+          _index: 24,
+          name: { 25: {} },
+          parent: {
+            26: {},
+            27: {},
+            28: {},
+            29: {},
+            30: {},
+            31: {},
+            32: {},
+            33: {},
+            34: {},
+            35: {},
+            36: {},
+            37: {},
+            38: {},
+            39: {},
+            40: {},
+            41: {},
+            42: {}
+          }
+        }
+      ],
+      [
+        "wallet",
+        {
+          _index: 16,
+          name: { 16: {}, 18: {}, 36: {} },
+          parent: { 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {} }
+        }
+      ]
+    ],
+    pipeline: []
+  }
+};
